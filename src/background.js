6c62d1e3b1ba873cd1b5c94cb827cecf4d3a8611
krasyuk2 <krasyuk.maksimka@gmail.com>
2026-08-27 18:42:16 +0700

    Немного разобрал основные методы chrome для работы

---
+++ src/background.js
@@ -0,0 +1,28 @@
+﻿
+chrome.runtime.onInstalled.addListener(async () => {
+    let oneSecond = 1 / 60;
+    console.log("Start alarm");
+    await chrome.alarms.create('check-video-timer',{
+        delayInMinutes: oneSecond * 5,
+        periodInMinutes: oneSecond * 5,
+        persistAcrossSessions: false // Сохранения состояния между сессиями
+    });
+});
+
+let a = 10;
+
+// Это, чтобы слать на сервер данные о просмотре - например каждый 5 сек
+chrome.alarms.onAlarm.addListener((alarm) => { // Слушатель alarm - когда возникает событие выполняем метод
+    console.log(alarm.name + a++);
+});
+
+// Действие на активную вкладку - берет код переделывает в строку и кидает на страницу и там выполняет
+chrome.tabs.onActivated.addListener(async ({tabId}) => {
+    const tab = await chrome.tabs.get(tabId)
+
+    await chrome.scripting.executeScript({
+        target: {tabId: tab.id},
+        func: (title) => alert(`Click: ${title}`),
+        args: [tab.title]
+    }).catch(err => console.log(err))
+});
---
+++ src/popup/index.js
@@ -0,0 +1,6 @@
+﻿// это выполняется тогда, когда мы тыкаем на расширение и он выполняет на активной вкладке
+const [tab] = await chrome.tabs.query({active: true, currentWindow: true});
+chrome.scripting.executeScript({
+    target: {tabId: tab.id},
+    func: () => alert('Hello! Tab id')
+});
