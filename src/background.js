c240765f18af25e72a1155222f615b063a80b651
krasyuk2 <krasyuk.maksimka@gmail.com>
2026-08-28 20:19:56 +0700

    Все еще разбор функционала

--- src/background.js
+++ src/background.js
@@ -1,5 +1,4 @@
-﻿
-chrome.runtime.onInstalled.addListener(async () => {
+﻿chrome.runtime.onInstalled.addListener(async () => {
     let oneSecond = 1 / 60;
     console.log("Start alarm");
     await chrome.alarms.create('check-video-timer',{
@@ -9,11 +8,13 @@ chrome.runtime.onInstalled.addListener(async () => {
     });
 });
 
-let a = 10;
-
 // Это, чтобы слать на сервер данные о просмотре - например каждый 5 сек
-chrome.alarms.onAlarm.addListener((alarm) => { // Слушатель alarm - когда возникает событие выполняем метод
-    console.log(alarm.name + a++);
+chrome.alarms.onAlarm.addListener(async (alarm) => { // Слушатель alarm - когда возникает событие выполняем метод
+    const tab = await getCurrentTab();
+    await chrome.scripting.executeScript({
+        target: {tabId: tab.id}, // внедряем во все фреймы
+        func: getCountVideoFrame
+    }).catch(err => console.log(err))
 });
 
 // Действие на активную вкладку - берет код переделывает в строку и кидает на страницу и там выполняет
@@ -21,8 +22,32 @@ chrome.tabs.onActivated.addListener(async ({tabId}) => {
     const tab = await chrome.tabs.get(tabId)
 
     await chrome.scripting.executeScript({
-        target: {tabId: tab.id},
-        func: (title) => alert(`Click: ${title}`),
-        args: [tab.title]
+        target: {tabId: tab.id, allFrames: true}, // внедряем во все фреймы
+        func: getVideoPlayer
     }).catch(err => console.log(err))
 });
+
+function getVideoPlayer() {
+    let videoTags = document.getElementsByTagName("video");
+    let test = '';
+    for(let video of videoTags) {
+        test+= video.currentTime;
+    }
+    alert(test);
+}
+
+function getVideoPlayers() {
+    return document.getElementsByTagName("video");
+}
+
+async function getCurrentTab() {
+    let queryOptions = { active: true, lastFocusedWindow: true };
+    let [tab] = await chrome.tabs.query(queryOptions);
+    return tab;
+}
+
+function getCountVideoFrame() {
+    let video = document.getElementsByTagName("video");
+    let iframe = document.getElementsByTagName("iframe");
+    alert("video" + video.length + "iframe" + iframe.length);
+}
